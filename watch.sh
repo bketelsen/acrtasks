6aa62b0535bf4ce19e499f709b74c930db0efd15
Brian Ketelsen <bketelsen@gmail.com>
2018-09-25 14:22:35 -0400

    update registry name

--- watch.sh
+++ watch.sh
@@ -4,8 +4,10 @@ az acr task create \
     --registry $ACR_NAME \
     --name tasksimpleweb \
     --image simpleweb:{{.Run.ID}} \
-    --arg REGISTRY_NAME=$ACR_NAME.azurecr.io \
+    --arg REGISTRY_NAME=$ACR_NAME \
     --context https://github.com/$GIT_USER/acrtasks.git \
     --file Dockerfile-app \
     --branch master \
     --git-access-token $GIT_PAT
+
+az acr task run --registry $ACR_NAME --name tasksimpleweb
